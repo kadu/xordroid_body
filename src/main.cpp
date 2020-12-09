05cc5d062f6f2316cdf12ab0d8b9206c553924b8
kadu <kadu@kadu.com.br>
2020-12-08 21:11:33 -0300

    Fist Commit

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "kaduzius";
+const char* password = "umasenhabemlegal27";
+
+void setupOTA() {
+  // Porta padrao do ESP8266 para OTA eh 8266 - Voce pode mudar ser quiser, mas deixe indicado!
+  // ArduinoOTA.setPort(8266);
+
+  // O Hostname padrao eh esp8266-[ChipID], mas voce pode mudar com essa funcao
+  ArduinoOTA.setHostname("xordroid");
+
+  // Nenhuma senha eh pedida, mas voce pode dar mais seguranca pedindo uma senha pra gravar
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Inicio...");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("nFim!");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progresso: %u%%r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Erro [%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Autenticacao Falhou");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Falha no Inicio");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Falha na Conexao");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Falha na Recepcao");
+    else if (error == OTA_END_ERROR) Serial.println("Falha no Fim");
+  });
+  ArduinoOTA.begin();
+}
+
+void wifiStart() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Conexao falhou! Reiniciando...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.println("Pronto");
+  Serial.print("Endereco IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Iniciando...");
+  wifiStart();
+  setupOTA();
+}
+
+void loop() {
+  ArduinoOTA.handle();
+}
